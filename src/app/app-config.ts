60abd92c2cb3b03ed3152ff98f6b561d2ab1b6cd
Jimmy Yar <jimmy1817@hotmail.com>
2022-07-27 09:08:44 -0500

    [Jimmy] cambiar imagen de core-loading-screen

--- src/app/app-config.ts
+++ src/app/app-config.ts
@@ -18,7 +18,7 @@ export const coreConfig: CoreConfig = {
   app: {
     appName     : 'Global RedPyme',                                        // App Name
     appTitle    : 'GRP Ifis', // App Title
-    appLogoImage: 'assets/images/logo/logo.jpeg',                  // App Logo
+    appLogoImage: 'assets/images/logo/logo.png',                  // App Logo
   },
   layout: {
     skin  : 'default',                        // default, dark, bordered, semi-dark
